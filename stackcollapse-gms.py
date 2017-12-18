93c5bfdc3703ad00313e14b83cbe7984f1eb6f57
melvinzhang <melvinzhang@gmail.com>
2017-12-18 11:13:37 +0800

    python script to convert GMS profiler csv to folded stacks format used by https://github.com/brendangregg/FlameGraph

---
+++ stackcollapse-gms.py
@@ -0,0 +1,34 @@
+import sys
+
+stack = [''] * 1000
+frames = []
+
+# generate frames, a frame is a call stack + total time
+for line in sys.stdin:
+    leading_spaces = len(line) - len(line.lstrip())
+    level = leading_spaces // 6
+    tok = line.split(',')
+    
+    if len(tok) < 2 or not tok[1].isnumeric():
+        continue
+    
+    name = tok[0].lstrip()
+    time = int(tok[1])
+
+    stack[level] = name
+    frames.append( (stack[:level+1], time) )
+
+# compute time spent in that frame
+for i in range(len(frames)):
+    stack, total_time = frames[i]
+    child_time = 0
+    for j in range(i+1,len(frames)):
+        cstack, ctotal_time = frames[j]
+        if cstack[:len(stack)] != stack:
+            break
+        elif len(cstack) == len(stack) + 1:
+            child_time += ctotal_time
+
+    used_time = total_time - child_time
+
+    print(';'.join(stack), used_time)
